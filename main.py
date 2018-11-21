9abcfd9ded1f6440ae49b86e4817f00717646312
Kolya59 <kolya.nikitas@yandex.ru>
2018-11-22 01:35:15 +0500

    Correct interpolation files

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-import solvers
-from mpmath import *
-from sympy import *
-import sympy as sp
+from sympy.parsing.sympy_parser import parse_expr
+import solvers as solv
 import numpy as np
 
-var = 'test'
+n = 5
+
+var = 'var1'
 in_file = open((var + '_in'), 'r')
 out_file = open((var + '_out'), 'w')
 
-f = sp.parse_expr(in_file.input().replace('\n',''))
+# Ввод функции
+f = parse_expr(in_file.read().replace('\n',''))
+# Вычисление значений функции в точках
+(points_vector, values_vector) = solv.table_creating(f, 1.0, 2.0, n)
+# Вычисление полинома Ньютона
+newton_polynomial = solv.newton_method(points_vector, values_vector, n)
+# Вычисление полинома кубическими сплайнами
+splain_polynomial = solv.cube_splain(points_vector, values_vector)
+
+# Вывод точек, в которых значение функции известно
+out_file.write((points_vector.__repr__() + '\n' + values_vector.__repr__() + '\n'))
+# Вывод значений, с помощью метода Ньютона
+out_file.write(('Полином Ньютона: ' + newton_polynomial.__repr__() + '\n'))
+# Вывод значений, с помощью метода кубических сплайнов
+out_file.write(('Полином методом кубических сплайнов: ' + splain_polynomial.__repr__() + '\n'))
+
+in_file.close()
+out_file.close()
+quit()
--- solvers.py
+++ solvers.py
@@ -1,59 +1,69 @@
 from mpmath import *
 from sympy import *
-import sympy as sp
+from scipy import interpolate
 import numpy as np
+
 x = Symbol('x')
 
+
 # Построение таблицы
-def table_creating(f, start, finish, n, out):
-    value_matrix = np.matrix(dtype=float)
-    xi = f.subs(x, start)
+def table_creating(f, start, finish, n):
+    point_matrix = np.zeros(n + 1)
+    value_matrix = np.zeros(n + 1)
+    xi = start
     df = diff(f, x)
     h = (finish - start) / n
-    for i in range(0, n):
-        value_matrix[i,0] = xi
-        value_matrix[i,1] = df.subs(x, xi)
-        xi = start + i * h
-    return value_matrix
+    for i in range(0, n + 1):
+        point_matrix[i] = xi
+        value_matrix[i] = df.subs(x, xi)
+        xi += h
+    return point_matrix, value_matrix
+
 
 # Вычисление разделенной разности
-def split_difference(yi,yj,xi,xj):
+def split_difference(yi, yj, xi, xj):
     return (yj - yi) / (xj / xi)
 
+
 # Создание таблицы разделенных разностей
-def split_difference_table_creating(f, input_matrix):
+def split_difference_table_creating(x_points, y_points):
     # Создание таблицы
-    matrix_range = input_matrix.shape()[0]
-    output_matrix = np.matrix(shape=(matrix_range + 2, matrix_range))
+    n = len(x_points)
+    output_matrix = np.zeros([n, n], dtype=float)
     # Заполнение первых 2 столбцов таблицы данными из исхдной таблицы
-    for i in range(0, matrix_range):
-        output_matrix[0,i] = input_matrix[0,i]
-        output_matrix[1,i] = input_matrix[1,i]
+    for i in range(0, n):
+        output_matrix[i,0] = y_points[i]
     # Заполнение остальных столбцов
-    for i in range(2, matrix_range):
-        for j in range(0, matrix_range - i + 1):
-            output_matrix[i,j] = split_difference(output_matrix[1,i],
-                                                  output_matrix[1,j],
-                                                  output_matrix[0,i],
-                                                  output_matrix[0,j])
+    for j in range(1, n):
+        for i in range(j, n):
+            output_matrix[i, j] = float(output_matrix[i, j - 1] - output_matrix[i - 1, j - 1]) \
+                                  / float(x_points[i] - x_points[i - j])
     return output_matrix
 
 
-
 # Метод Ньютона
-def newton_method(f, input_matrix, out):
-    split_difference_table = split_difference_table_creating(f, input_matrix)
+def newton_method(x_points, y_points, n):
+    # Построение таблицы разделенных разностей
+    split_difference_table = split_difference_table_creating(x_points, y_points)
+    # Вычисление Полинома
+    newton_polynomial = 0
+    for i in range(0, n):
+        next_elem = split_difference_table[i, i]
+        for j in range(0, i):
+            next_elem *= (x - x_points[j])
+        newton_polynomial += next_elem
 
+    return newton_polynomial
 
 # Метод кубический сплайнов дефекта 1
-def cube_splain(f, start, finish, step, out):
-
+def cube_splain(x_points, y_points):
+    return interpolate.CubicSpline(x_points, y_points)
 
 # Дискрентое среднеквадратичное приближение
-def discrete_rms_approximation(out):
+# def discrete_rms_approximation(out):
 
 # Интегральное среднеквадратичное отклонение
-def integral_rms_approximation(out):
+# def integral_rms_approximation(out):
 
 # Метод обратного интерполирования
-def inverse_interpolation_method(out):
+# def inverse_interpolation_method(out):
