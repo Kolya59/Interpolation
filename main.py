983023e9782e3719b1ba8604cd94ddf2aeafad69
Kolya59 <kolya.nikitas@yandex.ru>
2018-11-22 02:54:26 +0500

    Correct interpolation files

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from sympy.parsing.sympy_parser import parse_expr
-import solvers as solv
 import numpy as np
+from sympy.parsing.sympy_parser import parse_expr
+from tabulate import *
+
+import solvers as solv
 
 n = 5
 
@@ -9,18 +11,21 @@ in_file = open((var + '_in'), 'r')
 out_file = open((var + '_out'), 'w')
 
 # Ввод функции
-f = parse_expr(in_file.read().replace('\n',''))
+f = parse_expr(in_file.read().replace('\n', ''))
 # Вычисление значений функции в точках
 (points_vector, values_vector) = solv.table_creating(f, 1.0, 2.0, n)
 # Вычисление полинома Ньютона
-newton_polynomial = solv.newton_method(points_vector, values_vector, n)
+newton_polynomial, out_table_newton = solv.newton_method(points_vector, values_vector, n)
 # Вычисление полинома кубическими сплайнами
-splain_polynomial = solv.cube_splain(points_vector, values_vector)
+splain_polynomial, out_table_splain = solv.cube_splain(points_vector, values_vector)
 
 # Вывод точек, в которых значение функции известно
-out_file.write((points_vector.__repr__() + '\n' + values_vector.__repr__() + '\n'))
+out_file.write(
+    tabulate(np.column_stack((points_vector.transpose(), values_vector.transpose())), headers=['x', 'f(x)']) + '\n\n')
 # Вывод значений, с помощью метода Ньютона
-out_file.write(('Полином Ньютона: ' + newton_polynomial.__repr__() + '\n'))
+out_file.write(('Полином Ньютона:\n' + tabulate(out_table_newton) + '\n\n' +
+                tabulate(solv.interpolation_analysis([1.1, ..., 1.9], [1.0, ..., 2.0], f, newton_polynomial),
+                         headers=['x', 'f(x)', 'Pn(x)', 'Delta', 'Оценка'])))
 # Вывод значений, с помощью метода кубических сплайнов
 out_file.write(('Полином методом кубических сплайнов: ' + splain_polynomial.__repr__() + '\n'))
 
--- solvers.py
+++ solvers.py
@@ -1,7 +1,7 @@
-from mpmath import *
-from sympy import *
-from scipy import interpolate
 import numpy as np
+from mpmath import *
+from scipy import interpolate
+from sympy import *
 
 x = Symbol('x')
 
@@ -20,6 +20,21 @@ def table_creating(f, start, finish, n):
     return point_matrix, value_matrix
 
 
+# Анализ результатов интерполяции
+def interpolation_analysis(x_points, start_x_points, f, inter_f):
+    result_table = np.zeros((len(x_points), 5), dtype=float)
+    for i in range(len(x_points)):
+        result_table[i, 0] = x_points[i]
+        result_table[i, 1] = f.subs(x, x_points[i])
+        result_table[i, 2] = inter_f[i].subs(x, x_points[i])
+        result_table[i, 3] = abs(result_table[i, 1] - result_table[i, 2])
+        w = 1
+        for k in range(i + 1):
+            w *= x_points[i] - start_x_points[k]
+        result_table[i, 4] = abs(diff(f, x, i).subs(x, x_points[i]) / factorial(i + 1) * w)
+    return result_table
+
+
 # Вычисление разделенной разности
 def split_difference(yi, yj, xi, xj):
     return (yj - yi) / (xj / xi)
@@ -32,7 +47,7 @@ def split_difference_table_creating(x_points, y_points):
     output_matrix = np.zeros([n, n], dtype=float)
     # Заполнение первых 2 столбцов таблицы данными из исхдной таблицы
     for i in range(0, n):
-        output_matrix[i,0] = y_points[i]
+        output_matrix[i, 0] = y_points[i]
     # Заполнение остальных столбцов
     for j in range(1, n):
         for i in range(j, n):
@@ -47,17 +62,22 @@ def newton_method(x_points, y_points, n):
     split_difference_table = split_difference_table_creating(x_points, y_points)
     # Вычисление Полинома
     newton_polynomial = 0
+    # TODO: Доделать корректное типизирвоание для sympy_exr
+    newton_polynomial_list = np.zeros(n, dtype=)
     for i in range(0, n):
         next_elem = split_difference_table[i, i]
         for j in range(0, i):
             next_elem *= (x - x_points[j])
         newton_polynomial += next_elem
+        newton_polynomial_list[i] = (newton_polynomial)
+    out_table = np.column_stack((x_points.transpose(), split_difference_table))
+    return newton_polynomial_list, out_table
 
-    return newton_polynomial
 
 # Метод кубический сплайнов дефекта 1
 def cube_splain(x_points, y_points):
-    return interpolate.CubicSpline(x_points, y_points)
+    out_table_splain = []
+    return interpolate.CubicSpline(x_points, y_points), out_table_splain
 
 # Дискрентое среднеквадратичное приближение
 # def discrete_rms_approximation(out):
